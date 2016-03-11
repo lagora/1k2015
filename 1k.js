83f1961e3c353c2ae00b546bd48ed7bca061c746
Franck Lagora <lagora@users.noreply.github.com>
2016-03-11 17:27:46 +0100

    Added files via upload

--- 1k.js
+++ 1k.js
@@ -6,21 +6,22 @@ function start() {
   var centerY = Math.round(canvas.height / 2);
   var hiCanvasPosition = unitSize;
   var loCanvasPosition = canvas.height - unitSize;
-  var rect = function (position, size, color) {
-    context.fillStyle = "rgba("+[color.red, color.green, color.bleu, color.alpha].join(',')+")";
-    context.fillRect(position.x, position.y, size.w, size.h);
+  context.rect = function (position, size, color) {
+    this.fillStyle = "rgba("+[color.r, color.g, color.b, color.a].join(',')+")";
+    this.fillRect(position.x, position.y, size.w, size.h);
   };
   var player = {
+    x:centerX,
+    y:loCanvasPosition,
     w:unitSize,
     h:unitSize,
     r:192,
     g:168,
     b:0,
-    a:1
+    a:.25
   };
   var spawnPlayer = function (x) {
-    rect({x:x, y:loCanvasPosition}, player, player);
+    context.rect(player, player, player);
   };
-  var testColoCanvasPosition = 136;
-  rect({x:32, y:32}, {w:16, h:64}, {r:testColoCanvasPosition, g:testColoCanvasPosition, b:testColoCanvasPosition, a:0.5});
+  spawnPlayer(centerX);
 }
